5ef803bb793f88c0022306c8e05ed6d28a6a84f9
dflynt <flyntdw@unc.edu>
2018-01-11 17:12:46 -0500

    Commented and formatted changes

--- DJIA_SP500_NASDAQ_Scrape.py
+++ DJIA_SP500_NASDAQ_Scrape.py
@@ -1,10 +1,15 @@
-import requests, bs4
+import requests, bs4, csv
+stocks = []
 
+'''
+The data for both the S&P500 and the Dow Jones Industrial Average is
+gathered from their respective wikipedia pages and then added to the above
+stocks list.
+'''
 djiaStock = requests.get("https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average")
 djiaStock.raise_for_status()
 djiaStockSoup = bs4.BeautifulSoup(djiaStock.text, "html.parser")
 tableElem = djiaStockSoup.find_all('table', class_="wikitable sortable" )[0]
-stocks = []
 
 for row in tableElem.findAll('tr')[1:]:
     company = ""
@@ -25,16 +30,19 @@ for row in tableElem.findAll('tr')[1:]:
     company = company.split(",")
     stocks.append(company[2])
 
-import csv
+#NASDAQ data is retrieved from a CSV file
 with open("../companylist.csv", 'r') as csvfile:
     stockReader = csv.reader(csvfile)
     for row in stockReader:
         stocks.append(row[1])
+csvfile.close()
 
+#Removes duplicates and sorts the final result. I could have ran through the list and put unique values
+#in a map but programmers are lazy.
 stocks = set(stocks)
 stocks = list(stocks)
 stocks.sort()
-print(stocks)
+
 with open('Stocks.txt', 'w') as f:
     for x in stocks:
         f.write(x + "\n")
